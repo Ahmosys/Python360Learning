9cf14067d012bf372a5a4d07138225347d9899e0
Ahmosys <ahmosyspro@protonmail.com>
2022-09-24 22:18:50 +0200

    📦 NEW: Adding discord.py wrapper

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
 from modules import scrapper
 
+load_dotenv()
+client = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
-if __name__ == "__main__":
+
+@client.event
+async def on_ready():
+    await client.change_presence(
+        status=discord.Status.online, activity=discord.Game("./ahmosys#6967")
+    )
+
+@client.command(name="timetable", alias=["edt", "schedule"], description="Retrieve the timetable of the current week.")
+@commands.cooldown(1, 15, commands.BucketType.user)
+async def get_timetable(ctx: commands.Context):
     driver, driver_wait = scrapper.init()
+    message = await ctx.send("```**Attempt to login to SSO of 360Learning...**```")
     scrapper.login(driver=driver)
+    await message.edit(content="```**Switch from homepage to timetable page...**```")
     scrapper.get_timetable_page(driver=driver, driver_wait=driver_wait)
+    await message.edit(content="```**Generating the screenshot...**```")
     scrapper.get_screenshot(driver=driver)
+    await message.delete()
+    await ctx.send(file=discord.File("edt.png"))
+    
+
+client.run(os.getenv("DISCORD_TOKEN"))
--- modules/scrapper.py
+++ modules/scrapper.py
@@ -11,33 +11,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 def init():
     load_dotenv()
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(message)s",
+        datefmt="%d-%b-%y %H:%M:%S",
+    )
     s = Service("chromedriver.exe")
     opt = webdriver.ChromeOptions()
     opt.add_argument("headless")
     opt.add_experimental_option("excludeSwitches", ["enable-logging"])
     driver = webdriver.Chrome(service=s, options=opt)
-    driver_wait = WebDriverWait(driver, 10) # Timeout
+    driver_wait = WebDriverWait(driver, 10)  # Timeout value (explicit wait)
     return driver, driver_wait
 
 
 def login(driver: webdriver.Chrome):
-    logging.info("Attempt to login to SSO")
+    logging.debug("Attempt to login to SSO")
     driver.get("https://reseau-cd.360learning.com/")
     driver.find_element(By.ID, "ssoButton").click()
     driver.find_element(By.ID, "username").send_keys(os.getenv("USER"))
     driver.find_element(By.ID, "password").send_keys(os.getenv("PASS"))
     driver.find_element(By.NAME, "submit").click()
+    return True
 
 
 def get_timetable_page(driver: webdriver.Chrome, driver_wait: WebDriverWait):
-    logging.info("Switch to the timetable page")
-    driver_wait.until(ec.visibility_of_element_located((By.CLASS_NAME, "custom-link-container"))).click()
+    logging.debug("Switch to the timetable page")
+    driver_wait.until(
+        ec.visibility_of_element_located((By.CLASS_NAME, "custom-link-container"))
+    ).click()
     driver.switch_to.window(driver.window_handles[1])
+    return True
 
 
 def get_screenshot(driver: webdriver.Chrome):
-    logging.info("Loading of the timetable page according to the date defined")
-    # driver.get(driver.current_url.replace("09/24/2022", "10/22/2022"))
-    logging.info("Taking the screenshot")
-    driver.get_screenshot_as_file('edt.png')
+    logging.debug("Taking the screenshot")
+    driver.get(driver.current_url.replace("09/24/2022", "10/22/2022"))
+    driver.get_screenshot_as_file("edt.png")
+    return True
